5f6fe4d853726106abacac0e6b67d9ccab9e8cc6
JunePhattarawut <khunjune.p@gmail.com>
2017-07-18 18:00:33 +0700

    Add files via upload
    ed

--- bot.php
+++ bot.php
@@ -29,8 +29,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 }else if($arrJson['events'][0]['message']['text'] == "ที่นี่ที่ไหน"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "String";
-  $arrPostData['messages'][0]['String'] = "คุณอยู่ที่".$arrJson['source']['location'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['location'];
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
